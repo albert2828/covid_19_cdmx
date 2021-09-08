be5b553cbad0e6fdc4c5f0ab3676be2ff3daa9cb
Alberto Macias <alberto.macbar9512@gmail.com>
2021-09-07 23:08:01 -0500

    algunos archivos inecesarios eliminados

--- covid_data.py
+++ covid_data.py
@@ -6,7 +6,7 @@ from time import time
 
 #Formato fecha YYMMDD
 
-def covid_data_downloader(fecha):
+def descarga_base_covid(fecha):
     """
     Descarga la base de datos, del día de hoy, sobre covid 19 y la descomprime
     """
@@ -28,7 +28,7 @@ def covid_data_downloader(fecha):
             print("Descarga exitosa")
             print(f"La descarga tomó {total_time} segundos")
 
-def covid_old_data_downloader(fecha):
+def descarga_base_covid_antigua(fecha):
     """Descarga la base de datos de una fecha anterior a la del día actual"""
     zipname = "data/"+fecha+"COVID19MEXICO.zip"
     filename = fecha+"COVID19MEXICO.csv"
@@ -50,7 +50,7 @@ def covid_old_data_downloader(fecha):
             print("Descarga exitosa")
             print(f"La descarga tomó {total_time} segundos")
 
-def read_covid_data(fecha):
+def lee_base_covid(fecha):
     """
     Lee la base de datos de la carpeta y la carga como un data frame
     """
@@ -68,3 +68,35 @@ def read_covid_data(fecha):
 
     print(f"La base de datos tardó en cargarse {total_time} segundos")
     return df
+
+def defuncion(x):
+    '''Función que indica si el paciente falleció o no'''
+    if x == '9999-99-99':
+        d = 0
+    else:
+        d=1
+    return d
+
+def date_na(date):
+    if date == "9999-99-99":
+        date = "1999-01-31"
+    return date
+
+def procesa_datos_covid(fecha):
+    df = lee_base_covid(fecha)
+    df = df[df['ENTIDAD_RES']==9]
+    df = df[df['RESULTADO_LAB']==1]
+    #Agremaos una columna al dataframe que indique si el paciente falleció
+    df['DEF'] = df['FECHA_DEF'].apply(defuncion)
+    features = ['SEXO', 'TIPO_PACIENTE',
+       'FECHA_INGRESO', 'FECHA_SINTOMAS', 'FECHA_DEF', 'INTUBADO', 'NEUMONIA',
+       'EDAD', 'EMBARAZO','DIABETES', 'EPOC', 'ASMA', 'INMUSUPR', 'HIPERTENSION'
+       , 'OTRA_COM','CARDIOVASCULAR', 'OBESIDAD', 'RENAL_CRONICA', 'TABAQUISMO',
+        'DEF']
+    df = df[features]
+    df["FECHA_DEF"] = df["FECHA_DEF"].apply(date_na)
+    df["FECHA_SINTOMAS"] = pd.to_datetime(df["FECHA_SINTOMAS"])
+    df["FECHA_DEF"] = pd.to_datetime(df["FECHA_DEF"])
+    #Indicamos si el paciente fue hospitalizado.
+    df_gam["TIPO_PACIENTE"] = df_gam["TIPO_PACIENTE"].apply(lambda x: x-1)
+    df_gam["EMBARAZO"]=df_gam["EMBARAZO"].apply(lambda x: x-1)
